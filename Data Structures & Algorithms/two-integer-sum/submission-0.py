a88914db880d9bcba49ebe9b83baa348624d5372
Anmol Gupta <129684189+anmolguptacse@users.noreply.github.com>
2026-06-18 22:27:52 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp={}
+        for i,num in enumerate(nums):
+            complement=target-num
+            if complement in mp:
+                return [mp[complement],i]
+            
+            mp[num] = i
+        return [0,0]
